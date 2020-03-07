73fa5822d97e814daa50de817c8ca960cfa0802d
Daniel Ramos de Oliveira <drojf@hotmail.com>
2020-03-07 16:52:56 -0300

    Add project files.

---
+++ CamadaBLL/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CamadaBLL
+{
+    public class Class1
+    {
+    }
+}
